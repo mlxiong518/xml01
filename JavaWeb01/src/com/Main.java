4a8116a52ebea5d88ff8a944b1a35308951a659f
pgly8d <Mingliang_Xiong.sgmext@SAIC-GM.com>
2019-06-16 15:54:33 +0800

    first commit

---
+++ JavaWeb01/src/com/Main.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @author xiong
+ * @Title:
+ * @Description:
+ * @date 2019/6/14
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
